f7bda5cf8e16ad11640684b1b03f1d4ee2a1649e
Niklas Lorenz <50243091+niklaslorenz@users.noreply.github.com>
2022-05-08 18:45:07 +0200

    Output file is now specified on the command line

--- app/src/main/java/qpra/app/Main.java
+++ app/src/main/java/qpra/app/Main.java
@@ -7,28 +7,34 @@ import qpra.parser.qdimacs.QdimacsTreeParser;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        if(args.length != 1) {
+        if(args.length == 0 || args.length > 2) {
             System.out.println("Usage:\n <qpra> <input-file> [<output-file>]");
             return;
         }
-        File f = new File(args[0]);
-        if(!f.exists()) {
+        File inputFile = new File(args[0]);
+        if(!inputFile.exists()) {
             System.err.println("Input file does not exist.");
             return;
         }
-        QsatInstance instance = new QdimacsTreeParser().parse(f);
+        File outputFile = args.length > 1 ? new File(args[1]) : getDefaultOutputPath(inputFile.toPath()).toFile();
+        QsatInstance instance = new QdimacsTreeParser().parse(inputFile);
         if(instance == null) {
             System.err.println("Parsing Error. Could not interpret input.");
             return;
         }
         QsatFormatter formatter = new QsatFormatter();
-        File out = f.toPath().toAbsolutePath().getParent().resolve(f.getName() + ".qlp").toFile();
         String format = formatter.toYasolInput(instance);
-        Files.writeString(out.toPath(), format);
+        Files.writeString(outputFile.toPath(), format);
         System.out.println("Converted input into QLP format");
-        System.out.println("Wrote output to " + out.getPath());
+        System.out.println("Wrote output to " + outputFile.getPath());
     }
+
+    private static Path getDefaultOutputPath(Path inputPath) {
+        return inputPath.toAbsolutePath().getParent().resolve(inputPath.getFileName() + ".qlp");
+    }
+
 }
